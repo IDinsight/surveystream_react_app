c677be27fe7c6d68fede9626717d513c597f3129
Griffin Muteti <griffinmuteti31@gmail.com>
2023-06-09 12:32:55 +0530

    Integrate Surveys Home Page Screen & SCTO variable upload Screens (#8)

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,7 +2,10 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="flex h-[44px] bg-geekblue-9 justify-center items-center">
+    <footer
+      className="flex h-[44px] bg-geekblue-9 justify-center items-center"
+      style={{ position: "fixed", bottom: 0, width: "100%" }}
+    >
       <p className="font-inter font-normal text-sm leading-[22px] text-gray-1">
         IDinsight 2023 &#169;
       </p>
---
+++ src/redux/surveyList/types.ts
@@ -0,0 +1,13 @@
+export type Survey = {
+  survey_uid: number;
+  survey_id: string;
+  survey_name: string;
+  project_name: string;
+  survey_description: string;
+  survey_method: string;
+  irb_approval: string;
+  planned_start_date: string;
+  planned_end_date: string;
+  state: string;
+  last_updated_at: string;
+};
