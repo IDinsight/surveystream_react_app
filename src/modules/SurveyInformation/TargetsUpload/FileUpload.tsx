44aaf4e34226f20d9674e700746166f351817b34
Griffin Muteti <griffinmuteti31@gmail.com>
2023-09-08 11:11:12 +0300

    Missing Headers / Duplicate Columns
    - remove column name checks
    - move all common csv validator logic to a util helper file
    - use the same logic to validate both targets and enumerator csvs
    - identify missing header names
    - identify column duplicates

--- src/modules/SurveyInformation/TargetsUpload/FileUpload.tsx
+++ src/modules/SurveyInformation/TargetsUpload/FileUpload.tsx
@@ -7,6 +7,10 @@ import CSVFileValidator, {
 } from "csv-file-validator";
 
 import { Buffer } from "buffer";
+import {
+  classifyErrorsForColumns,
+  validateCSVData,
+} from "../../../utils/csvValidator";
 const { Dragger } = Upload;
 
 interface IFileUpload {
@@ -22,17 +26,6 @@ interface IFileUpload {
   setErrorList: Dispatch<SetStateAction<any>>;
 }
 
-interface ErrorObject {
-  rowIndex?: number;
-  columnIndex?: number;
-  message: string;
-}
-
-interface ErrorSummary {
-  type: string;
-  count: number;
-  message: string[];
-}
 function FileUpload({
   setUploadStatus,
   onFileUpload,
@@ -43,140 +36,7 @@ function FileUpload({
 }: IFileUpload & React.ComponentProps<typeof Dragger>) {
   const [fileList, setFileList] = useState([]);
 
-  function isEmailValid(email: string): boolean {
-    const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-    return emailPattern.test(email);
-  }
-
-  const csvValidationRules: FieldSchema[] = [
-    {
-      inputName: "Target ID",
-      name: "target_id",
-      required: true,
-      unique: true,
-      requiredError: function (headerName, rowNumber, columnNumber) {
-        return `${headerName} is required in the ${rowNumber} row / ${columnNumber} column`;
-      },
-      validateError: function (headerName, rowNumber, columnNumber) {
-        return `${headerName} is not valid in the ${rowNumber} row / ${columnNumber} column`;
-      },
-    },
-    {
-      inputName: "Location ID",
-      name: "location_id",
-      optional: true,
-      requiredError: function (headerName, rowNumber, columnNumber) {
-        return `${headerName} is required in the ${rowNumber} row / ${columnNumber} column`;
-      },
-      validateError: function (headerName, rowNumber, columnNumber) {
-        return `${headerName} is not valid in the ${rowNumber} row / ${columnNumber} column`;
-      },
-    },
-    { inputName: "Language", name: "language", optional: true },
-    {
-      inputName: "Gender",
-      name: "gender",
-      optional: true,
-      requiredError: function (headerName, rowNumber, columnNumber) {
-        return `${headerName} is required in the ${rowNumber} row / ${columnNumber} column`;
-      },
-      validateError: function (headerName, rowNumber, columnNumber) {
-        return `${headerName} is not valid in the ${rowNumber} row / ${columnNumber} column`;
-      },
-    },
-  ];
-
-  const validateCSVData = async (file: File) => {
-    console.log("file", file);
-    // Implement this to cover all fields
-    const validationConfig: ValidatorConfig = {
-      headers: csvValidationRules,
-      isColumnIndexAlphabetic: true,
-    };
-
-    try {
-      const validationResults = await CSVFileValidator(file, validationConfig);
-
-      return validationResults;
-    } catch (error: any) {
-      message.error("Validation went wrong");
-    }
-  };
-
-  function classifyErrorsForColumns(
-    errorList: ErrorObject[],
-    rules: FieldSchema[]
-  ): ErrorSummary[] {
-    const errorSummaryMap: { [key: string]: ErrorSummary } = {};
-
-    for (const error of errorList) {
-      let matchedRule: FieldSchema | undefined = undefined;
-      for (const rule of rules) {
-        if (error.message.includes(rule.name)) {
-          matchedRule = rule;
-          break;
-        }
-      }
-
-      if (!matchedRule) {
-        const errorType = "Column names not found";
-        const errorMessage = error.message;
-        if (!errorSummaryMap[errorType]) {
-          errorSummaryMap[errorType] = {
-            type: errorType,
-            count: 1,
-            message: [errorMessage],
-          };
-        } else {
-          errorSummaryMap[errorType].count++;
-          errorSummaryMap[errorType].message.push(errorMessage);
-        }
-        continue;
-      }
-
-      let errorType: string;
-      let errorMessage: string;
-      if (
-        matchedRule.required &&
-        (error.message.includes(
-          `The Header name should be ${matchedRule.name}`
-        ) ||
-          error.message.includes(
-            `${matchedRule.name} is not correct or missing`
-          ))
-      ) {
-        errorType = "Template Errors";
-        errorMessage = error.message;
-      } else if (
-        matchedRule.required &&
-        error.message.includes(`${matchedRule.name} is not valid`)
-      ) {
-        errorType = "Validation Errors";
-        errorMessage = error.message;
-      } else if (
-        matchedRule.unique &&
-        error.message.includes(`${matchedRule.name} is not unique`)
-      ) {
-        errorType = `Duplicate Errors`;
-        errorMessage = error.message;
-      } else {
-        continue; // Skip errors that don't match the specified conditions
-      }
-
-      if (!errorSummaryMap[errorType]) {
-        errorSummaryMap[errorType] = {
-          type: errorType,
-          count: 1,
-          message: [errorMessage],
-        };
-      } else {
-        errorSummaryMap[errorType].count++;
-        errorSummaryMap[errorType].message.push(errorMessage);
-      }
-    }
-
-    return Object.values(errorSummaryMap);
-  }
+  const csvValidationRules: FieldSchema[] = [];
 
   const clearUpload = () => {
     setFileList([]);
@@ -216,15 +76,18 @@ function FileUpload({
 
     const result = await validateCSVData(file);
 
-    console.log("validateCSVData", result);
+    if (result) {
+      console.log("validateCSVData", result.inValidData.length > 0);
+    }
 
-    if (result?.inValidData && result.inValidData.length > 0) {
-      const errors: ErrorObject[] = result.inValidData.filter((error) => {
-        const isMismatchedFieldsError = error.message.startsWith(
-          "Number of fields mismatch:"
-        );
-        return !isMismatchedFieldsError;
-      });
+    if (result !== undefined && result.inValidData.length > 0) {
+      const errors: string[] = [];
+
+      for (const error of result.inValidData) {
+        if (typeof error === "string") {
+          errors.push(error);
+        }
+      }
 
       console.log("errors", errors);
 
@@ -240,23 +103,6 @@ function FileUpload({
         return false;
       }
     }
-    reader.onload = () => {
-      const csvData = reader.result as string;
-      const encodedData = csvData.split(",")[1]; // Extract the base64 data
-      const decodedData = Buffer.from(encodedData, "base64").toString(); // Decode the base64 data
-      const rows = decodedData.split("\n");
-      const columnNames = rows[0]
-        .split(",")
-        .map((columnName) => columnName.trim()); // Trim the column names
-
-      setTimeout(() => {
-        onFileUpload(file, columnNames, rows, encodedData);
-        onSuccess("ok", new XMLHttpRequest());
-        message.success(`${file.name} file uploaded successfully.`);
-        setUploadStatus(true);
-      }, 1000);
-    };
-    reader.readAsDataURL(file); // Use the 'file' object directly
   };
 
   const handleDrop = (e: React.DragEvent) => {
---
+++ src/utils/csvValidator.ts
@@ -0,0 +1,148 @@
+interface ValidationResult {
+  isValid: boolean;
+  inValidData: string[];
+}
+import CSVFileValidator, {
+  FieldSchema,
+  ValidatorConfig,
+} from "csv-file-validator";
+import { message } from "antd";
+
+interface ErrorSummary {
+  type: string;
+  count: number;
+  message: string[];
+}
+
+const csvValidationRules: FieldSchema[] = [];
+
+export const isEmailValid = (email: string): boolean => {
+  const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+  return emailPattern.test(email);
+};
+
+export const validateHeaders = (headers: string[]): string[] => {
+  const errors: string[] = [];
+
+  // Assuming headers should not be empty
+  if (headers.length === 0 || headers.some((header) => header.trim() === "")) {
+    errors.push("Headers are missing or empty.");
+  }
+
+  return errors;
+};
+
+export const readFileAsText = (file: File): Promise<string> => {
+  return new Promise<string>((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onload = (event) => {
+      if (event.target && event.target.result) {
+        resolve(event.target.result as string);
+      } else {
+        reject(new Error("Failed to read the file."));
+      }
+    };
+    reader.readAsText(file);
+  });
+};
+export const findDuplicateColumns = (headers: string[]): string[] => {
+  const duplicateColumns: string[] = [];
+  const seenColumns: Set<string> = new Set();
+
+  for (const header of headers) {
+    const trimmedHeader = header.trim(); // Remove leading/trailing whitespace
+    if (seenColumns.has(trimmedHeader)) {
+      duplicateColumns.push(trimmedHeader);
+    } else {
+      seenColumns.add(trimmedHeader);
+    }
+  }
+
+  return duplicateColumns;
+};
+
+export const countEmptyRows = (lines: string[]): number => {
+  return lines.slice(1).filter((line) => line.trim() === "").length;
+};
+
+export const displayValidationErrors = (errors: string[]): ValidationResult => {
+  return { isValid: false, inValidData: errors };
+};
+
+export const classifyErrorsForColumns = (
+  errorList: string[],
+  rules: FieldSchema[]
+): ErrorSummary[] => {
+  const errorSummaryMap: { [key: string]: ErrorSummary } = {};
+
+  for (const error of errorList) {
+    if (error.includes("Duplicate columns")) {
+      const errorType = "Duplicate Columns";
+      const errorMessage = error;
+      if (!errorSummaryMap[errorType]) {
+        errorSummaryMap[errorType] = {
+          type: errorType,
+          count: 1,
+          message: [errorMessage],
+        };
+      } else {
+        errorSummaryMap[errorType].count++;
+        errorSummaryMap[errorType].message.push(errorMessage);
+      }
+      continue;
+    }
+
+    const errorType = "Validation Errors";
+    const errorMessage: string = error;
+
+    if (!errorSummaryMap[errorType]) {
+      errorSummaryMap[errorType] = {
+        type: errorType,
+        count: 1,
+        message: [errorMessage],
+      };
+    } else {
+      errorSummaryMap[errorType].count++;
+      errorSummaryMap[errorType].message.push(errorMessage);
+    }
+  }
+
+  return Object.values(errorSummaryMap);
+};
+
+export const validateCSVData = async (file: File) => {
+  console.log("file", file);
+  const text = await readFileAsText(file);
+  const lines = text.split("\n");
+  const headers = lines[0].split(",");
+
+  const headerResults = validateHeaders(headers);
+  if (headerResults.length > 0) {
+    return displayValidationErrors(headerResults);
+  }
+
+  const duplicateColumns = findDuplicateColumns(headers);
+
+  console.log("duplicateColumns", duplicateColumns);
+
+  if (duplicateColumns.length > 0) {
+    return displayValidationErrors([
+      `Duplicate columns found: ${duplicateColumns.join(", ")}`,
+    ]);
+  }
+
+  // Implement this to cover all fields
+  const validationConfig: ValidatorConfig = {
+    headers: csvValidationRules,
+    isHeaderNameOptional: false,
+    isColumnIndexAlphabetic: true,
+  };
+
+  try {
+    const validationResults = await CSVFileValidator(file, validationConfig);
+
+    return validationResults;
+  } catch (error: any) {
+    message.error("Validation went wrong");
+  }
+};
