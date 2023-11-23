9e1e63cb2c6aafea264463cd38380896a910273d
Griffin Muteti <griffinmuteti31@gmail.com>
2023-11-23 14:39:37 +0300

    - remove dropdown on user management

--- src/components/NavItems/NavItems.tsx
+++ src/components/NavItems/NavItems.tsx
@@ -6,22 +6,36 @@ import {
   UsergroupAddOutlined,
 } from "@ant-design/icons";
 import "./NavItems.css";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const NavItems = () => {
+  const location = useLocation();
+
   return (
     <div className="nav-menu flex">
-      <div className="nav-menu-item w-36 bg-geekblue-5">
+      <div
+        className={`nav-menu-item w-36 ${
+          location.pathname.startsWith("/surveys") ? "bg-geekblue-5" : ""
+        }`}
+      >
         <HomeFilled className="flex items-center !text-base !text-gray-2" />
-        <span className="!text-gray-2">Surveys</span>
+        <Link to="/surveys">
+          <span className="!text-gray-2">Surveys</span>
+        </Link>
       </div>
-      <div className="nav-menu-item">
+      <div
+        className={`nav-menu-item ${
+          location.pathname.startsWith("/users") ? "bg-geekblue-5" : ""
+        }`}
+      >
         <div>
           <ApartmentOutlined className="flex items-center !text-base !text-gray-2" />
-          <span className="!text-gray-2">User management</span>
-          <CaretDownOutlined className="nav-dropdown-tooltip" />
+          <Link to="/users">
+            <span className="!text-gray-2">User management</span>
+            {/* <CaretDownOutlined className="nav-dropdown-tooltip" /> */}
+          </Link>
         </div>
-        <div className="nav-submenu">
+        {/* <div className="nav-submenu">
           <Link to="/users">
             <UsergroupAddOutlined className="flex items-center" />
             <span>Users</span>
@@ -30,7 +44,7 @@ const NavItems = () => {
             <IdcardOutlined className="flex items-center" />
             <span>Roles</span>
           </Link>
-        </div>
+        </div> */}
       </div>
     </div>
   );
