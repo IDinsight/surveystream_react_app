f244c3bfcffa267bb231761c36e4f5fa3986e9e9
Jayprakash <0freerunning@gmail.com>
2023-11-23 16:35:57 +0530

    [SS-1307] Fix active state on header nagivation item (#132)

--- src/components/NavItems/NavItems.tsx
+++ src/components/NavItems/NavItems.tsx
@@ -8,14 +8,21 @@ import {
 import "./NavItems.css";
 import { Link } from "react-router-dom";
 
+const isActiveItem = (path: string) => {
+  const currentPath = location.pathname;
+  return currentPath.includes(path) ? "bg-geekblue-5" : "";
+};
+
 const NavItems = () => {
   return (
     <div className="nav-menu flex">
-      <div className="nav-menu-item w-36 bg-geekblue-5">
-        <HomeFilled className="flex items-center !text-base !text-gray-2" />
-        <span className="!text-gray-2">Surveys</span>
+      <div className={"nav-menu-item  w-36 " + isActiveItem("surveys")}>
+        <Link to="/surveys">
+          <HomeFilled className="flex items-center !text-base !text-gray-2" />
+          <span className="!text-gray-2">Surveys</span>
+        </Link>
       </div>
-      <div className="nav-menu-item">
+      <div className={"nav-menu-item " + isActiveItem("users")}>
         <div>
           <ApartmentOutlined className="flex items-center !text-base !text-gray-2" />
           <span className="!text-gray-2">User management</span>
