73277fe374ae3c1309d876ac7844f37e23b9c6c8
Jay Prakash <0freerunning@gmail.com>
2024-05-10 01:12:43 +0300

    Screen 2

--- src/modules/SurveyInformation/SurveyStatusMapping/SurveyStatusMapping.styled.tsx
+++ src/modules/SurveyInformation/SurveyStatusMapping/SurveyStatusMapping.styled.tsx
@@ -1,7 +1,20 @@
+import { Button } from "antd";
 import styled from "styled-components";
 
 export const TextHeading = styled.h1`
   font-size: 24px;
   font-weight: 500;
   color: #000;
+`;
+
+export const ConfirmBtn = styled(Button)`
+  background-color: #2f54eb;
+  color: white;
+  border-radius: 8px !important;
+  min-width: 94px !important;
+
+  &:hover {
+    background-color: #2f54eb !important;
+    color: white !important;
+  }
 `;
--- src/modules/SurveyInformation/SurveyStatusMapping/SurveyStatusMapping.tsx
+++ src/modules/SurveyInformation/SurveyStatusMapping/SurveyStatusMapping.tsx
@@ -1,18 +1,36 @@
-import { useNavigate } from "react-router-dom";
-import { useAppDispatch } from "../../../redux/hooks";
+import { useNavigate, useParams } from "react-router-dom";
+import { useAppDispatch, useAppSelector } from "../../../redux/hooks";
 import Container from "../../../components/Layout/Container";
 import FullScreenLoader from "../../../components/Loaders/FullScreenLoader";
 import NavItems from "../../../components/NavItems";
 import Header from "../../../components/Header";
 import { Button, Form, Input, Select } from "antd";
 import { useForm } from "antd/es/form/Form";
+import { useEffect } from "react";
+import { getSurveyCTOForm } from "../../../redux/surveyCTOInformation/surveyCTOInformationActions";
+import { RootState } from "../../../redux/store";
+import { ConfirmBtn } from "./SurveyStatusMapping.styled";
 
 function SurveyStatusMapping() {
   const navigate = useNavigate();
   const dispatch = useAppDispatch();
   const [form] = useForm();
 
-  const isLoading = false;
+  const { survey_uid } = useParams<{ survey_uid?: string }>() ?? {
+    survey_uid: "",
+  };
+
+  const { loading: isLoading, surveyCTOForm: sctoForm } = useAppSelector(
+    (state: RootState) => state.surveyCTOInformation
+  );
+
+  useEffect(() => {
+    if (!survey_uid) {
+      navigate("/surveys");
+    }
+
+    dispatch(getSurveyCTOForm({ survey_uid: survey_uid }));
+  }, [dispatch, survey_uid]);
 
   return (
     <>
@@ -31,21 +49,37 @@ function SurveyStatusMapping() {
             >
               <Form.Item
                 label="SCTO form ID"
-                name="password"
+                name="scto-form-id"
                 required
                 tooltip="This is a required field"
                 rules={[
-                  { required: true, message: "Please input new password!" },
+                  { required: true, message: "Please select the form id!" },
                 ]}
               >
                 <Select placeholder="Select SCTO form ID" allowClear>
-                  <Select.Option value="scto-form">SCTO form</Select.Option>
+                  {Object.keys(sctoForm).length > 0 ? (
+                    <Select.Option value={sctoForm.scto_form_id}>
+                      {sctoForm.scto_form_id}
+                    </Select.Option>
+                  ) : null}
                 </Select>
               </Form.Item>
 
-              <Form.Item shouldUpdate>
-                <Button type="primary">Next</Button>
-              </Form.Item>
+              {form.getFieldValue("scto-form-id") ? (
+                <>
+                  <Form.Item
+                    label="Survey modality"
+                    name="survey-modality"
+                    required
+                    tooltip="This is a required field"
+                  >
+                    <Input defaultValue="In-person" disabled />
+                  </Form.Item>
+                  <Form.Item shouldUpdate>
+                    <ConfirmBtn type="primary">Confirm</ConfirmBtn>
+                  </Form.Item>
+                </>
+              ) : null}
             </Form>
           </div>
         </>
