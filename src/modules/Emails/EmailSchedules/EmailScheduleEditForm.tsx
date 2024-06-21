360f7f141c5453d4e0704d3de458c31c8037e5c5
Griffin Muteti <griffinmuteti31@gmail.com>
2024-06-21 12:28:03 +0300

    update edit schedule form to handle a single schedule

--- src/modules/Emails/EmailSchedules/EmailScheduleEditForm.tsx
+++ src/modules/Emails/EmailSchedules/EmailScheduleEditForm.tsx
@@ -8,15 +8,11 @@ import {
   DatePicker,
   TimePicker,
 } from "antd";
-import { MinusCircleOutlined } from "@ant-design/icons";
 import { useAppDispatch, useAppSelector } from "../../../redux/hooks";
-import {
-  updateEmailSchedule,
-  getEmailSchedules,
-} from "../../../redux/emails/emailsActions";
+import { updateEmailSchedule } from "../../../redux/emails/emailsActions";
 import { RootState } from "../../../redux/store";
 import FullScreenLoader from "../../../components/Loaders/FullScreenLoader";
-import moment from "moment";
+import dayjs from "dayjs";
 
 const { Option } = Select;
 const { RangePicker } = DatePicker;
@@ -25,6 +21,7 @@ const EmailScheduleEditForm = ({
   handleBack,
   handleContinue,
   initialValues,
+  fetchEmailSchedules,
 }: any) => {
   const [form] = Form.useForm();
   const dispatch = useAppDispatch();
@@ -33,13 +30,8 @@ const EmailScheduleEditForm = ({
   const [loading, setLoading] = useState(false);
 
   const formatDate = (date: any) => {
-    const d = new Date(date);
-    const month = `${d.getMonth() + 1}`.padStart(2, "0");
-    const day = `${d.getDate()}`.padStart(2, "0");
-    const year = d.getFullYear();
-    return `${year}-${month}-${day}`;
+    return dayjs(date).format("YYYY-MM-DD");
   };
-
   const generateDateRange = (start: any, end: any, frequency: any) => {
     const dates = [];
     const currentDate = new Date(start);
@@ -74,77 +66,108 @@ const EmailScheduleEditForm = ({
   const handleSubmit = async () => {
     setLoading(true);
     try {
-      const formValues = await form.validateFields();
-      const { schedules } = form.getFieldsValue();
+      form.validateFields();
+      const formValues = await form.getFieldsValue();
+      const { emailScheduleName, dateType, dates, emailTime, emailFrequency } =
+        formValues;
 
-      for (let i = 0; i < schedules.length; i++) {
-        const schedule = schedules[i];
+      if (!dateType) {
+        message.error("Date type is required");
+        setLoading(false);
 
-        const formattedDates = formatDates(schedule);
-        const formattedTime = schedule?.emailTime.format("HH:mm");
-
-        const emailScheduleData = {
-          dates: formattedDates,
-          time: formattedTime,
-          email_schedule_name: schedule?.emailScheduleName,
-        };
-
-        console.log("emailScheduleData", emailScheduleData);
-
-        const emailScheduleUID = initialValues.email_schedule_uid;
-
-        const res = await dispatch(
-          updateEmailSchedule({
-            id: emailScheduleUID,
-            emailScheduleData: emailScheduleData,
-          })
-        );
-
-        console.log("updateEmailSchedule res", res);
-
-        if (!res.payload.success) {
-          // Error occurred
-          message.error(
-            res.payload?.message
-              ? res.payload?.message
-              : "An error occurred, email schedules could not be updated. Kindly check form data and try again"
-          );
-          setLoading(false);
-          return;
-        }
+        return;
       }
 
-      message.success("Email schedules updated successfully");
+      if (!emailFrequency && dateType == "multiple") {
+        message.error("Email frequency is required");
+        setLoading(false);
+
+        return;
+      }
+
+      console.log("formValues", formValues);
+
+      const schedule = {
+        dateType: dateType,
+        dates: dates,
+        emailFrequency: emailFrequency,
+      };
+
+      console.log("schedule", schedule);
+
+      const formattedDates = formatDates(schedule);
+      const formattedTime = emailTime.format("HH:mm");
+
+      const emailScheduleData = {
+        dates: formattedDates,
+        time: formattedTime,
+        email_schedule_name: emailScheduleName,
+      };
+
+      console.log("emailScheduleData", emailScheduleData);
+
+      const emailScheduleUID = initialValues.email_schedule_uid;
+
+      const res = await dispatch(
+        updateEmailSchedule({
+          id: emailScheduleUID,
+          emailScheduleData: emailScheduleData,
+        })
+      );
+
+      console.log("updateEmailSchedule res", res);
+
+      if (!res.payload.success) {
+        // Error occurred
+        message.error(
+          res.payload?.message
+            ? res.payload?.message
+            : "An error occurred, email schedule could not be updated. Kindly check form data and try again"
+        );
+        setLoading(false);
+        return;
+      }
+
+      message.success("Email schedule updated successfully");
+      fetchEmailSchedules();
     } catch (error) {
       console.error("error", error);
-      message.error("Failed to update email schedules");
+      message.error("Failed to update email schedule");
     }
     setLoading(false);
   };
 
-  // Helper function to format dates
   const formatDates = (schedule: any) => {
+    console.log("schedule", schedule);
     return schedule.dateType === "multiple"
       ? generateDateRange(
           schedule.dates[0],
-          schedule.dates[1],
+          schedule.dates[-1],
           schedule.emailFrequency
         )
       : [schedule.dates.format("YYYY-MM-DD")];
   };
 
   useEffect(() => {
+    console.log("initialValues", initialValues);
+
     if (initialValues) {
-      form.setFieldsValue({
-        schedules: initialValues.map((schedule: any) => ({
-          ...schedule,
-          dates:
-            schedule.dateType === "multiple"
-              ? [moment(schedule.dates[0]), moment(schedule.dates[1])]
-              : moment(schedule.dates),
-          emailTime: moment(schedule.time, "HH:mm"),
-        })),
-      });
+      const dateType = initialValues.dates.length > 1 ? "multiple" : "single";
+
+      const formValues = {
+        dateType: dateType,
+        emailScheduleName: initialValues.email_schedule_name,
+        dates:
+          dateType === "multiple"
+            ? [dayjs(initialValues.dates[0]), dayjs(initialValues.dates[-1])]
+            : dayjs(initialValues.dates),
+        emailTime: dayjs(initialValues.time, "HH:mm"),
+        emailFrequency: initialValues.emailFrequency,
+      };
+
+      console.log("formValues", formValues);
+
+      form.setFieldsValue({ ...formValues });
     }
   }, [initialValues]);
 
@@ -154,179 +177,89 @@ const EmailScheduleEditForm = ({
 
   return (
     <Form form={form} layout="vertical">
-      <Form.List name="schedules">
-        {(fields, { remove }) => (
-          <>
-            {fields.map(({ key, name, ...restField }) => (
-              <div key={key} style={{ marginBottom: 8 }}>
-                {fields.length > 1 && (
-                  <MinusCircleOutlined
-                    onClick={() => remove(name)}
-                    style={{ float: "right" }}
-                  />
-                )}
-                <Form.Item
-                  {...restField}
-                  name={[name, "emailScheduleName"]}
-                  label="Email Schedule Name"
-                  rules={[
-                    {
-                      required: true,
-                      message: "Please enter the email schedule name",
-                    },
-                  ]}
-                >
-                  <Input placeholder="Email Schedule Name e.g. Morning, Midday, Evening" />
-                </Form.Item>
-                <div style={{ display: "flex" }}>
-                  <Form.Item
-                    style={{ width: "40%", marginRight: "5px" }}
-                    {...restField}
-                    name={[name, "dateType"]}
-                    rules={[
-                      { required: true, message: "Please select a date type" },
-                    ]}
-                  >
-                    <Select placeholder="Select Date Type">
-                      <Select.Option value="single">Single Date</Select.Option>
-                      <Select.Option value="multiple">
-                        Multiple Dates
-                      </Select.Option>
-                    </Select>
-                  </Form.Item>
-                  <Form.Item
-                    style={{ width: "60%", marginRight: "5px" }}
-                    shouldUpdate={(prevValues, currentValues) =>
-                      prevValues.schedules !== currentValues.schedules
-                    }
-                  >
-                    {({ getFieldValue }) => {
-                      const dateType = getFieldValue([
-                        "schedules",
-                        name,
-                        "dateType",
-                      ]);
-                      return dateType === "multiple" ? (
-                        <Form.Item
-                          {...restField}
-                          name={[name, "dates"]}
-                          rules={[
-                            { required: true, message: "Please select dates" },
-                          ]}
-                        >
-                          <RangePicker
-                            placeholder={["Start Date", "End Date"]}
-                            format="YYYY-MM-DD"
-                          />
-                        </Form.Item>
-                      ) : (
-                        <Form.Item
-                          {...restField}
-                          name={[name, "dates"]}
-                          rules={[
-                            { required: true, message: "Please select a date" },
-                          ]}
-                        >
-                          <DatePicker
-                            placeholder="Select Date"
-                            format="YYYY-MM-DD"
-                          />
-                        </Form.Item>
-                      );
-                    }}
-                  </Form.Item>
-                </div>
-                <div style={{ display: "flex" }}>
-                  <Form.Item
-                    style={{ width: "40%", marginRight: "5px" }}
-                    {...restField}
-                    name={[name, "emailTime"]}
-                    label="Email Time"
-                    rules={[
-                      { required: true, message: "Please select a time" },
-                    ]}
-                  >
-                    <TimePicker placeholder="Select Time" format="HH:mm" />
-                  </Form.Item>
+      <Form.Item
+        name="emailScheduleName"
+        label="Email Schedule Name"
+        rules={[
+          {
+            required: true,
+            message: "Please enter the email schedule name",
+          },
+        ]}
+      >
+        <Input placeholder="Email Schedule Name e.g. Morning, Midday, Evening" />
+      </Form.Item>
 
-                  <Form.Item
-                    noStyle
-                    shouldUpdate={(prevValues, currentValues) =>
-                      prevValues.schedules !== currentValues.schedules
-                    }
-                  >
-                    {({ getFieldValue }) => {
-                      const dateType = getFieldValue([
-                        "schedules",
-                        name,
-                        "dateType",
-                      ]);
-                      return dateType === "multiple" ? (
-                        <Form.Item
-                          style={{ width: "40%", marginRight: "5px" }}
-                          {...restField}
-                          name={[name, "emailFrequency"]}
-                          label="Email Frequency"
-                          rules={[
-                            {
-                              required: true,
-                              message: "Please select the email frequency",
-                            },
-                          ]}
-                        >
-                          <Select placeholder="Select Email Frequency">
-                            <Select.Option value="daily">Daily</Select.Option>
-                            <Select.Option value="weekly">Weekly</Select.Option>
-                            <Select.Option value="bi_weekly">
-                              Bi-Weekly
-                            </Select.Option>
-                            <Select.Option value="monthly">
-                              Monthly
-                            </Select.Option>
-                            <Select.Option value="annually">
-                              Annually
-                            </Select.Option>
-                          </Select>
-                        </Form.Item>
-                      ) : null;
-                    }}
-                  </Form.Item>
-                </div>
-              </div>
-            ))}
-          </>
-        )}
-      </Form.List>
+      <Form.Item
+        name="dateType"
+        label="Date Type"
+        rules={[{ required: true, message: "Please select a date type" }]}
+      >
+        <Select placeholder="Select Date Type">
+          <Option value="single">Single Date</Option>
+          <Option value="multiple">Multiple Dates</Option>
+        </Select>
+      </Form.Item>
+
+      <Form.Item shouldUpdate>
+        {({ getFieldValue }) => {
+          const dateType = getFieldValue("dateType");
+          return dateType === "multiple" ? (
+            <Form.Item
+              name="dates"
+              label="Dates"
+              rules={[{ required: true, message: "Please select dates" }]}
+            >
+              <RangePicker
+                placeholder={["Start Date", "End Date"]}
+                format="YYYY-MM-DD"
+              />
+            </Form.Item>
+          ) : (
+            <Form.Item
+              name="dates"
+              label="Date"
+              rules={[{ required: true, message: "Please select a date" }]}
+            >
+              <DatePicker placeholder="Select Date" format="YYYY-MM-DD" />
+            </Form.Item>
+          );
+        }}
+      </Form.Item>
+
+      {form.getFieldValue("dateType") === "multiple" && (
+        <Form.Item
+          name="emailFrequency"
+          label="Email Frequency"
+          rules={[
+            {
+              required: true,
+              message: "Please select the email frequency",
+            },
+          ]}
+        >
+          <Select placeholder="Select Email Frequency">
+            <Option value="daily">Daily</Option>
+            <Option value="weekly">Weekly</Option>
+            <Option value="bi_weekly">Bi-Weekly</Option>
+            <Option value="monthly">Monthly</Option>
+            <Option value="annually">Annually</Option>
+          </Select>
+        </Form.Item>
+      )}
+
+      <Form.Item
+        name="emailTime"
+        label="Email Time"
+        rules={[{ required: true, message: "Please select a time" }]}
+      >
+        <TimePicker placeholder="Select Time" format="HH:mm" />
+      </Form.Item>
 
       <div style={{ display: "flex", marginTop: "40px" }}>
         <Button
-          style={{
-            display: "flex",
-            marginRight: "35%",
-          }}
-          loading={loading}
-          onClick={handleBack}
-        >
-          Back
-        </Button>
-
-        <Button
-          style={{
-            display: "flex",
-            marginRight: "35%",
-          }}
-          loading={loading}
-          onClick={handleContinue}
-        >
-          Skip
-        </Button>
-
-        <Button
           type="primary"
-          style={{
-            display: "flex",
-            backgroundColor: "#3AB44E",
-          }}
+          onSubmit={() => form.validateFields()}
           onClick={handleSubmit}
         >
           Submit
