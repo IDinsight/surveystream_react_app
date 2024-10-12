e6ae6c50dd11ff505b54923d32bee63a15fd094c
Jeenu Thomas <jeenuthomas27@gmail.com>
2024-10-12 11:03:53 +0530

    Minor change to delete button hover colors (#274)

--- src/components/MediaForm/MediaForm.styled.ts
+++ src/components/MediaForm/MediaForm.styled.ts
@@ -5,7 +5,7 @@ export const DeleteBtn = styled(Button)`
   margin-top: 24px;
   margin-left: 12px;
   &:hover {
-    background-color: red;
-    color: white !important;
+    background-color: white !important;
+    color: red !important;
   }
 `;
